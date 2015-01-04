74898f77519ded69e8d27f02aa60bf5b27a05ec0
gamahead <gamahead@gmail.com>
2015-01-04 02:20:15 -0500

    fixed background image

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  match '/test', to: 'home#test', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
